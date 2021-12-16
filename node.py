9073952c08c8cf71499423ae490abbd8b05e64f2
Shubhrajit Sinha <sinhashubhrajit@gmail.com>
2021-12-16 03:18:16 -0800

    finished the node

--- node.py
+++ node.py
@@ -1,5 +1,17 @@
 import rospy
 from geometry_msgs.msg import Twist
-rospy.init_node('myController')
 
-pub = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10)
+rospy.init_node('myController')  # Creating a node
+
+pub = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10) # create a publisher
+
+msg = Twist()
+
+msg.linear.x =1
+
+while not rospy.is_shutdown():
+	pub.publish(msg)
+
+print('yes')
+
+
